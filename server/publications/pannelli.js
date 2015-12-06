892cf5b8970da68820243c1ee835475ba6828c30
Massimiliano Masserelli <massimiliano.masserelli@essepi.eu>
2015-12-06 10:32:08 +0100

    Aggiornata regola publish per sorting case insensitive

--- server/publications/pannelli.js
+++ server/publications/pannelli.js
@@ -1,10 +1,10 @@
 Meteor.publish( 'pannelli', function() {
-  return Pannelli.find( { enabled: true }, { fields: { 'nome': 1 } } );
+  return Pannelli.find( { enabled: true }, { fields: { 'nome': 1, 'name_sort': 1  } } );
 });
 Meteor.publish("singlePanel", function(panelID) {
   check(panelID, String);
   // Make a delay manually to show the loading state
   Meteor._sleepForMs(2000);
 
-  return Pannelli.find({_id:panelID, enabled: true }, { fields: { 'nome': 1, 'password': 1 }});
+  return Pannelli.find({_id:panelID, enabled: true }, { fields: { 'nome': 1, 'password': 1}});
 });
